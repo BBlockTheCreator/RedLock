2676890b74ec9e16d4feaa96083974f1c6fb351a
BBlockTheCreator <benso1601@gmail.com>
2025-09-06 14:01:19 +0200

    Update payload.py

--- payload.py
+++ payload.py
@@ -1,3 +1,9 @@
-print("this could've been Malware")
-print("be more careful in the future")
-input()
+import ctypes
+
+# 0x40 = Info-Icon
+ctypes.windll.user32.MessageBoxW(
+    0,
+    "Be more careful!\nThis could've been malware.",
+    "Info",
+    0x40
+)
